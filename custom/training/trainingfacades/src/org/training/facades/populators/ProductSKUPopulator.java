a71aaf1cacd9ccdd482b7e70e20f946c7ceea116
Agung Teja Pratama <agungteja64@gmail.com>
2021-01-25 13:50:04 +0700

    First Commit

---
+++ custom/training/trainingfacades/src/org/training/facades/populators/ProductSKUPopulator.java
@@ -0,0 +1,19 @@
+package org.training.facades.populators;
+
+import de.hybris.platform.commercefacades.product.data.ProductData;
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.model.product.ProductModel;
+import org.apache.commons.beanutils.ConversionException;
+import org.training.core.model.TrainingVariantProductModel;
+
+public class ProductSKUPopulator implements Populator<ProductModel, ProductData> {
+
+    @Override
+    public void populate(ProductModel source, ProductData target) throws ConversionException {
+        // Set Product data property here
+        if (source instanceof TrainingVariantProductModel) {
+            TrainingVariantProductModel trainingVariantProductModel = (TrainingVariantProductModel) source;
+            target.setSku(trainingVariantProductModel.getSku());
+        }
+    }
+}
